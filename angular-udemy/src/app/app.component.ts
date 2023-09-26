61a37705ba7c984abd96c634def6759c7ff89772
Lucas Gauto <lukscgauto@gmail.com>
2023-09-26 14:41:20 -0300

    Section 3(componentes) finished.

--- angular-udemy/src/app/app.component.ts
+++ angular-udemy/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-udemy';
+export class AppComponent implements OnInit{
+
+  public destruir: boolean = true;
+
+  constructor(){}
+
+  ngOnInit(): void {}
+  
+  public destruirComponente(){
+    this.destruir = false
+  }
+
 }
